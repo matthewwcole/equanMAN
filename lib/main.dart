3ddf80cc0d49f2144fce8e63e29f237d06484e11
Matthew <mr.mwcole@gmail.com>
2026-02-05 23:37:03 -0500

    this is a test branch

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
           displayColor: Pallete.primaryText,
         ),
       ),
-      title: '😠 Equanimity 😮‍💨',
+      title: '😠 Equanimity',
       home: Banner(
         message: "BETA",
         location: BannerLocation.topEnd,
